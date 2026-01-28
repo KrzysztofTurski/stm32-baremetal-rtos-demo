459a1f1d8c5dc739b8bbbb178964f2b6a8b6e6d7
krzysztof turski <krzysztof.turski40@gmail.com>
2026-01-28 07:11:36 +0100

    Refactor LPS25HB header

--- core/drivers/sensors/lps25hb.h
+++ core/drivers/sensors/lps25hb.h
@@ -1,11 +1,29 @@
+#ifndef LPS25HB_H
+#define LPS25HB_H
 
-#include "stm32f4xx.h"
 #include <stdint.h>
+#include "stm32f4xx.h"
 
+/*
+ * LPS25HB – czujnik ciśnienia i temperatury
+ */
 
 #define LPS25HB_WHOAMI_EXPECTED  0xBD
 
+/* Inicjalizacja czujnika */
 void LPS25HB_init(void);
+
+/* Odczyt ciśnienia (surowa wartość lub przeliczona – zależnie od implementacji) */
 uint32_t LPS25HB_readPressure(void);
+
+/* Odczyt temperatury */
 int16_t LPS25HB_readTemperature(void);
+
+/* Odczyt rejestru WHO_AM_I */
 uint8_t LPS25HB_readWhoAmI(void);
+
+#endif /* LPS25HB_H */
+
+
+
+
